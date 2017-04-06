1cfcf37e1d0c1222c142734372b1cd76bc7be14f
c757450 <constanzo.jason@heb.com>
2017-04-06 13:00:03 -0500

    asdf

--- script.js
+++ script.js
@@ -28,6 +28,8 @@
 	myApp.controller('mainController', function($scope) {
 		// create a message to display in our view
 		$scope.message = 'Everyone come and see how good I look!';
+
+		myApp.metaDescription = "This is the description yo";
 	});
 
 	myApp.controller('aboutController', function($scope) {
