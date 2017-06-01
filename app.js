f7f5bf2a6b4f222a6e1d24fcc7286a275dee543b
Jason Constanzo <constanzo.jason@heb.com>
2017-06-01 16:08:16 -0500

    - Ajax

--- app.js
+++ app.js
@@ -29,18 +29,10 @@ routerApp.config(function($stateProvider, $urlRouterProvider, $locationProvider)
                 // AJAX //
                 var url = "https://neblo.github.io/metadata.json";
                 $http.get(url).then( function(response) {
-                    //////////////////////////////////
-                    // simulate extra network delay //
-                    // console.log("Chilling");
-                    // var seconds = 3;
-                    // var start = +(new Date());
-                    // while (new Date() - start < seconds*1000);
-                    //
+                    // debug code //
                     console.log(response.data.title);
                     console.log(response.data.description);
-                    // console.log("Finished Delay");
-                    //////////////////////////////////
-
+                    ////////////////
                     PageService.setPageTitle(response.data.title);
                     PageService.setMetaDescription(response.data.description);
                 });
